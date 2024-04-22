2068a6917b0d7f13c7cc5e4804e75b0b8c973a5e
Szymon Piątek <szymon.piatek@pisf.pl>
2024-04-22 09:58:13 +0200

    Fix session

--- main.py
+++ main.py
@@ -10,19 +10,26 @@ class Session:
     def __init__(self):
         self.logged_in = False
         self.user = None
+        self.user_id = None
         self.user_username = None
 
     def login(self, username):
-        self.user = username
-        self.user_username = self.user[1]
         self.logged_in = True
+        self.user = username
+        self.user_id = self.user[0]
+        self.user_username = self.user[1]
 
     def logout(self):
-        self.user = None
         self.logged_in = False
+        self.user = None
+        self.user_id = None
+        self.user_username = None
 
     def is_logged_in(self):
         return self.logged_in
+
+    def get_user_id(self):
+        return self.user_id
 
     def get_username(self):
         return self.user_username
